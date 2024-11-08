5cb3d5a6599e0751ba6f1f9bd9db69e26ac4572b
Prasadraju99 <evaraprasadraju.azure@gmail.com>
2024-11-08 20:46:14 +0530

    terraform-aws-vpc

--- data.tf
+++ data.tf
@@ -2,11 +2,15 @@ data "aws_availability_zones" "available" {
   state = "available" # datasources to get data of availability zones from us-east-1
 }
 
-data "aws_vpc" "default" {  # to get data of the desired/specific vpc id
-  filter {
-    name   = "tag:Name"
-    values = ["Default-VPC"]
-  }
+# data "aws_vpc" "default" {  # to get data of the desired/specific vpc id
+#   filter {
+#     name   = "tag:Name"
+#     values = ["Default-VPC"]
+#   }
+# }
+
+data "aws_vpc" "default" {    # to get data/vpc_id from default VPC
+  default = true
 }
 
 data "aws_route_table" "main" {
